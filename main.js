d3dcb87aa96577a005da87a63eea4321c610dc9d
Muhammad Akhtar Shamim <shaminaus@gmail.com>
2017-01-19 12:27:00 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const electron = require('electron')
+var path = require('path')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    transparent: true,
+    frame: false,
+    toolbar: false,
+    resizable: false,
+    width:1300,
+    show: false,
+    icon: path.join(__dirname, 'images/ruler.png')
+  })
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
+}
+
+app.on('ready', createWindow)
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
